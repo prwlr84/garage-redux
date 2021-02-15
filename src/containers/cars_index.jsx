dd3e2952b237dc0d0d907e919bc3c822755391c4
Antal Bako <prowlersk8@gmail.com>
2021-02-15 12:23:13 +0100

    added a bit but mostly upgrade --latest, mode, use in config and webpack serve in package.json and babel7 not sure if its relevant though

---
+++ src/containers/cars_index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CarsIndex extends Component {
+
+
+  render(){
+    return(
+      <div className="main">
+
+      </div>
+    )
+  }
+}
+
+export default CarsIndex;
