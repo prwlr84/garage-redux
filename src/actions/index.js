cb291035c6ad40ad6312e10d06aed3e80c0ae57d
Antal Bako <prowlersk8@gmail.com>
2021-02-16 17:00:35 +0100

    posts car to garage, needs to fix custom garage and styling

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,20 @@
 // TODO: add and export your own actions
 export const FETCH_CARS = 'FETCH_CARS';
 export const FETCH_CAR = 'FETCH_CAR';
+export const NEW_CAR = 'NEW_CAR';
+
+export function newCar(garage, body, callback) {
+  const req = fetch(`https://wagon-garage-api.herokuapp.com/${garage}/cars`,{
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify(body)
+  }).then(r => r.json())
+    .then(callback);
+  return {
+    type: NEW_CAR,
+    payload: req
+ }
+}
 
 export function fetchCar(id) {
   const car = fetch(`https://wagon-garage-api.herokuapp.com/cars/${id}`)
@@ -19,4 +33,4 @@ export function fetchCars(garage) {
     payload: cars
  }
 }
-//damians-cars
+
